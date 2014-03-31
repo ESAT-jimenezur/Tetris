b50fec647cc651c61e6c96d4b51652cd16630afb
iJos <joseluiszero@gmail.com>
2014-03-31 19:00:13 +0200

    Añadidos colores por defecto y titulo de ventana a la parte de administracion

--- source/admin.cpp
+++ source/admin.cpp
@@ -5,10 +5,44 @@
 
 
 
+/* Definimos los métodos necesarios */
+
+/**
+    Este método se encarga de mostrar el menu por pantalla
+**/
+void menu();
+
+/**
+    Este método se encarga de establecer los colores necesarios para la ejecucion
+**/
+void setColors();
 
 
+
+
+
+void setColors(int texto, int fondo){
+    colorTexto(texto, fondo);
+}
+
+void menu(){
+
+    printf("*** ------------ *** iJosTris Admin Backend *** ------------ ***\n");
+
+}
 
 int main(){
+    /* Para la correcta ejecución de este juego, debemos poner la consola en "Lucida Console" y 8 píxeles de tamaño */
+
+
+    //Nuestra ventana molona bien grande
+    ventanaConsola(155,73, "iJoStratego");
+
+    //Establecemos los colores iniciales
+    setColors(0, 14);
+
+
+    menu();
 
     return 0;
 }
