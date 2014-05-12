3b7b75608f36834b157b435324e1a6abe0375adf
iJos <joseluiszero@gmail.com>
2014-05-12 16:40:08 +0200

    Actualizando codigo de clase

--- source/admin.cpp
+++ source/admin.cpp
@@ -2,33 +2,302 @@
 #include <iJos.h>
 #include<conio.h>
 #include <string.h>
+#include <windows.h>
 
 
+struct Fecha{
+    char dia[10];
+    char mes[10];
+    char anyo[10];
+};
 
-/* Definimos los métodos necesarios */
+struct Lugar{
+    char provincia[20];
+    char pais[20];
+};
 
-/**
-    Este método se encarga de mostrar el menu por pantalla
-**/
+struct Jugador{
+    char usuario[20];
+    char pass[20];
+    char nombre[20];
+    char apellido1[20];
+    char apellido2[20];
+    char email[40];
+    int fichas;
+    bool activo;
+    Fecha fecha_nacimiento;
+    Lugar lugar_nacimiento;
+};
+
+Jugador jugador;
+FILE *fichero_datos;
+FILE *fichero_auxiliar;
+
+
+/** Definimos los métodos necesarios **/
+
+/**Este método se encarga de mostrar el menu por pantalla**/
 void menu();
 
-/**
-    Este método se encarga de establecer los colores necesarios para la ejecucion
-**/
+/**Este método se encarga de establecer los colores necesarios para la ejecucion**/
 void setColors();
+
+/**Este método se encarga de insertar nuevos jugadores**/
+void alta_jugador();
+
+/**Este método se encarga de pedir datos para hacer altas**/
+void pedirDatos();
+
+/**Este método se encarga mostrar la lista de jugadores**/
+void alta_jugador();
+
+/**Este método se encarga modificar jugadores**/
+void modifica_jugador();
+
+/**Este método se encarga eliminar jugadores**/
+void elimina_jugador();
 
 
 void setColors(int texto, int fondo){
     colorTexto(texto, fondo);
 }
 
-void menu(){
 
-    //printf("*** ------------ *** iJosTris Admin Backend *** ------------ ***\n\n\n");
+void pedirDatos(){
 
-    //ventana(0, 0, 4, 3, 0);
+    system("cls");
+    fflush(stdin);
+    printf("*** Nuevo Jugador ***\n");
+    printf("Introduce nombre de usuario: ");
+        scanf("%s", &jugador.usuario);
+        fflush(stdin);
+
+    printf("Introduce una contrasena: ");
+        scanf("%s", &jugador.pass);
+        fflush(stdin);
+
+    printf("Introduce el email: ");
+        scanf("%s", &jugador.email);
+        fflush(stdin);
+
+    printf("Introduce un nombre: ");
+        scanf("%s", &jugador.nombre);
+        fflush(stdin);
+
+    printf("Introduce el primer apellido: ");
+        scanf("%s", &jugador.apellido1);
+        fflush(stdin);
+
+    printf("Introduce un segundo apellido: ");
+        scanf("%s", &jugador.apellido2);
+        fflush(stdin);
+
+    printf("Introduce el DIA de nacimiento: ");
+        scanf("%s", &jugador.fecha_nacimiento.dia);
+        fflush(stdin);
+
+    printf("Introduce el MES de nacimiento: ");
+        scanf("%s", &jugador.fecha_nacimiento.mes);
+        fflush(stdin);
+
+     printf("Introduce el ANYO de nacimiento: ");
+        scanf("%s", &jugador.fecha_nacimiento.anyo);
+        fflush(stdin);
+
+
+    printf("Introduce la provincia: ");
+        scanf("%s", &jugador.lugar_nacimiento.provincia);
+        fflush(stdin);
+
+    printf("Introduce el pais: ");
+        scanf("%s", &jugador.lugar_nacimiento.pais);
+        fflush(stdin);
+
+        //Activamos el jugador y establecemos sus fichas
+        jugador.activo = true;
+        jugador.fichas = 10;
+}
+
+
+void alta_jugador(){
+
+    char seguir;
+
+    do{
+
+        pedirDatos();
+        fichero_datos = fopen("../database/players_database.dat", "ab");
+        fwrite(&jugador, sizeof(jugador), 1, fichero_datos);
+        fclose(fichero_datos);
+        printf("\n Quieres insertar otro jugador?\n S/N \n");
+        seguir = getch();
+
+    }while(seguir == 's');
+
+    //menu();
+}
+
+
+void lista_jugadores(){
+
+    system("cls");
+
+    fichero_datos = fopen("../database/players_database.dat", "rb");
+
+    printf("\n *************** Lista de Jugadores *************** \n\n");
+
+
+    while(!feof(fichero_datos)){
+    fread(&jugador, sizeof(jugador), 1, fichero_datos);
+    if(!feof(fichero_datos)){
+        printf("%s %s %s %s %s %c \n \n",
+           jugador.usuario,
+           jugador.nombre,
+           jugador.apellido1,
+           jugador.apellido2,
+           jugador.email,
+           jugador.fichas);
+        }
+    }
+
+
+    fclose(fichero_datos);
+    printf("\n *************** Pulsa cualquier tecla para salir *************** \n");
+    getch();
+    menu();
+}
+
+
+void modifica_jugador(){
+
+    system("cls");
+
+    char usuario[20];
+
+    fichero_datos = fopen("../database/players_database.dat", "r+");
+
+    printf("\n ***************        Modificar Jugador        *************** \n\n");
+    printf("Usuario del jugador a modificar: ");
+    scanf("%s", usuario);
+    fflush(stdin);
+
+    int apuntador = 0;
+
+    while(!feof(fichero_datos)){
+    fread(&jugador, sizeof(jugador), 1, fichero_datos);
+        if(!feof(fichero_datos)){
+            if(strcmp(strlwr(usuario), strlwr(jugador.usuario)) == 0){
+                printf("Contacto encontrado, modificando...");
+                getch();
+                fseek(fichero_datos, sizeof(jugador) * apuntador, SEEK_SET);
+                pedirDatos();
+                fwrite(&jugador, sizeof(jugador), 1, fichero_datos);
+                printf("Usuario modificado");
+                fclose(fichero_datos);
+                getch();
+                menu();
+            }else{
+                printf("No se ha encontrado el jugador");
+                fclose(fichero_datos);
+                getch();
+                menu();
+            }
+            apuntador++;
+        }
+    }
+
+    fclose(fichero_datos);
+    printf("\n *************** Pulsa cualquier tecla para salir *************** \n");
+    getch();
+    menu();
 
 }
+
+
+void elimina_jugador(){
+
+    system("cls");
+
+    char usuario[20];
+
+    fichero_auxiliar = fopen("../database/AUX_players_database.dat", "wb");
+    fichero_datos = fopen("../database/players_database.dat", "r+");
+
+    printf("\n ***************        Eliminar Jugador        *************** \n\n");
+    printf("Usuario del jugador a eliminar: ");
+    scanf("%s", usuario);
+    fflush(stdin);
+
+    int apuntador = 0;
+
+    while(!feof(fichero_datos)){
+    fread(&jugador, sizeof(jugador), 1, fichero_datos);
+        if(!feof(fichero_datos)){
+            if(strcmp(strlwr(usuario), strlwr(jugador.usuario)) == 0){
+                printf("Contacto encontrado, eliminando...\n");
+                getch();
+                fwrite(&jugador, sizeof(jugador), 1, fichero_auxiliar);
+                printf("Usuario eliminado\n");
+                fclose(fichero_datos);
+                fclose(fichero_auxiliar);
+                getch();
+            }
+            apuntador++;
+        }
+    }
+
+    fclose(fichero_datos);
+    fclose(fichero_auxiliar);
+
+    remove("../database/AUX_players_database.dat");
+    rename("../database/AUX_players_database.dat", "../database/players_database.dat");
+
+    printf("\n *************** Pulsa cualquier tecla para salir *************** \n");
+    getch();
+    menu();
+
+
+}
+
+
+void menu(){
+
+
+    int elec;
+
+    do{
+        system("cls");
+        printf("*** ------------ *** iJosTris Admin Backend *** ------------ ***\n\n\n");
+        printf("1.- Insertar nuevo jugador\n");
+        printf("2.- Eliminar jugador\n");
+        printf("3.- Modificar jugador\n");
+        printf("4.- Ver lista de jugadores\n");
+        printf("5.- Salir \n");
+
+        scanf("%d", &elec);
+
+
+        switch(elec){
+            case 1:
+                alta_jugador();
+                break;
+            case 2:
+                elimina_jugador();
+                break;
+            case 3:
+                modifica_jugador();
+                break;
+            case 4:
+                lista_jugadores();
+                break;
+            case 5:
+                return;
+        }
+
+    }while(elec != 6);
+
+}
+
 
 int main(){
     /* Para la correcta ejecución de este juego, debemos poner la consola en "fuente de mapa de bits" y 8x8 píxeles de tamaño */
--- source/tetris.cpp
+++ source/tetris.cpp
@@ -2,6 +2,30 @@
 #include <iJos.h>
 #include<conio.h>
 #include <string.h>
+#include <dictionary.h>
+#include <tetrominoes.h>
+
+
+
+
+
+/* Public Vars */
+const int anchoVentana      = 150;
+const int altoVentana       = 100;
+
+const int gameInterface_size_width    = 20;
+const int gameInterface_size_height   = 10;
+const int gameInterface_size_margin   = 2;
+const int gameInterface_position_X    = 10;
+const int gameInterface_position_Y    = 10;
+
+const int gameFicha_ascii_model       = 178;
+
+const int ficha_size                  = 4;
+int currentFicha_X                    = 0;
+int currentFicha_Y                    = 0;
+
+int game_zone_table[gameInterface_size_width][gameInterface_size_height];
 
 
 
@@ -16,54 +40,276 @@ void menu();
     Este método se encarga de establecer los colores necesarios para la ejecucion
 **/
 void setColors(int texto, int fondo);
+void drawGameInterface(int posX, int posY, int size);
+void insertFicha(int game_zone_table_X, int game_zone_table_Y);
+void drawFicha(int posX, int posY, int tipoFicha);
+void clearBlock(int posX, int opsY, int size);
 
-void drawScreen(int posX, int posY){
-
-    //cursorPos(posX, posY);
-
-    for(int i = 0; i < 22; i++){
-        for(int j = 0; j < 12; j++){
-            if(i == 0 || i == 21 || j == 0 || j == 11){
-                setColors(0, 12);
-            }else{
-                setColors(0, 14);
-            }
-            printf("0");
+void loader_game_zone_table(){
+    for(int i = 0; i < gameInterface_size_width; i++){
+        for(int j = 0; j < gameInterface_size_height; j++){
+            game_zone_table[i][j] = 0;
         }
-        printf("\n");
     }
 }
 
 
+void convierteTextoAASCII(int posX, int posY, char* titulo){
+
+    cursorPos(posX, posY);
+
+    // Tratar cadena para descomprimir por caracter
+    // Convertir cada caracter a ASCII
+    // Imprimir caracter en ascii
+
+    for(int le = 0;  le < strlen(titulo); le++){
+        printf("%c", titulo[le]);
+        // Aqui convertimos
+        /*
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 5; j++){
+                //A[i][j]
+
+
+                if(letra[i][j] == 1){
+                    printf("%c", 219);
+                }else{
+                    printf(" ");
+                }
+
+            }
+
+            posY++;
+            printf("\n");
+            cursorPos(posX, posY);
+        }
+        */
+
+    }
+
+}
+
+void pantallaJuego(){
+    // Este metodo se llama 1 vez y pinta toda la zona de juego
+    // Establecemos el color de fondo
+    system("color 80");
+
+    char titulo[10] = "Tetris";
+
+
+    //Cargamos el array de juego
+    loader_game_zone_table();
+
+    //convierteTextoAASCII(anchoVentana/2 - (strlen(titulo)/2), 2, titulo);
+
+    //insertFicha(0, 0);
+    drawGameInterface(gameInterface_position_X, gameInterface_position_Y, ficha_size);
+    //YEAH --> drawFicha(0, 0, 1);
+
+    // Tiempo Inicial
+    struct tm *HoraFechaActual;
+    time_t tiempo=time(NULL);
+    int hh_ini,mm_ini,ss_ini;
+    char tecla;
+    time (&tiempo);
+    HoraFechaActual = localtime(&tiempo);
+
+    hh_ini=HoraFechaActual->tm_hour;
+    mm_ini=HoraFechaActual->tm_min;
+    ss_ini=HoraFechaActual->tm_sec;
+
+    int drawPos_y = 0;
+    int posFicha_X = 10;
+    int posFicha_Y = 10;
+    while(true) {
+
+        //Calculamos la diferencia de tiempos
+        time_t tiempo=time(NULL);
+        time (&tiempo);
+        struct tm *HoraSiguiente = localtime(&tiempo);
+
+        //Esta es la estructura
+        //hh=HoraFechaActual->tm_hour;
+        //mm=HoraFechaActual->tm_min;
+        //ss=HoraFechaActual->tm_sec;
+
+
+        /*  Auto movimiento
+        int t_diff = 0;
+        if((HoraSiguiente->tm_sec - ss_ini) + t_diff == 1){
+            drawFicha(0, drawPos_y, 1);
+            drawPos_y ++;
+            clearLine(0, ficha_size);
+            t_diff++;
+        }
+        */
+
+
+        /* TO-DO*/
+        if(kbhit()){
+            tecla=getch();
+            clearBlock(posFicha_X, posFicha_Y, ficha_size);
+            if(tecla == 80){ // Abajo
+                drawFicha(posFicha_X, posFicha_Y, 1);
+                posFicha_Y++;
+            }else if(tecla == 72){ // Arriba
+                drawFicha(posFicha_X, posFicha_Y, 1);
+                posFicha_Y--;
+            }else if(tecla == 75){ // Izquierda
+                drawFicha(posFicha_X, posFicha_Y, 1);
+                posFicha_X--;
+            }else if(tecla == 77){ // Derecha
+                drawFicha(posFicha_X, posFicha_Y, 1);
+                posFicha_X++;
+            }
+
+        }
+
+    }
+}
+
+void drawGameInterface(int posX, int posY, int size = 1){
+
+    cursorPos(posX, posY);
+
+    int controller_width = 0;
+    for(int i = 0; i < (gameInterface_size_width + gameInterface_size_margin) * size; i++){
+
+        int controller_height = 0;
+        for(int j = 0; j < (gameInterface_size_height + gameInterface_size_margin) * size; j++){
+
+            /*if(i == 0 || i == 21 || j == 0 || j == 11){
+                //setColors(0, 12);
+            }else{
+                //setColors(0, 14);
+            }*/
+            setColors(0, 14);
+            /*
+
+            if(game_zone_table[controller_width][controller_height] == 1){
+                setColors(0, 9);
+            }else{
+                setColors(0, 14);
+            }
+
+            printf("%d", game_zone_table[controller_width][controller_height]);
+            controller_height++;
+            */
+            printf(" ");
+
+        }
+
+        printf("\n");
+
+        posY++;
+        controller_width++;
+        cursorPos(posX, posY);
+    }
+
+}
+/*
+void insertFicha(int game_zone_table_X, int game_zone_table_Y){
+    //La metemos en el array
+    for(int i = 0; i < 7; i++){
+        for(int j = 0; j < 5; j++){
+            if(game_zone_table[i+game_zone_table_X][j+game_zone_table_Y] == 0){ // Este if puede servir para no chafar fichas
+                game_zone_table[i+game_zone_table_X][j+game_zone_table_Y] = tetrominoe_1[i][j];
+            }else{
+
+            }
+
+        }
+    }
+
+    currentFicha_X = game_zone_table_X;
+    currentFicha_Y = game_zone_table_Y;
+}
+*/
+
+void clearBlock(int posX, int posY, int size){
+    //cursorPos(gameInterface_position_X + gameInterface_size_margin, gameInterface_position_Y + gameInterface_size_margin + (lineY-1);
+    //for(int j = 0; j < (gameInterface_size_width + gameInterface_size_margin) * size/2 ; j++){
+        //printf(" ");
+    //}
+    int localX = posX + gameInterface_position_X + gameInterface_size_margin;
+    int localY = posY + gameInterface_position_Y + gameInterface_size_margin;
+
+    cursorPos(localX, localY); // Esto ajusta el borrado de la linea anterior
+
+    for(int i = 0; i < 7; i++){
+        for(int j = 0; j < 5; j++){
+            printf("3");
+        }
+        //printf("\n");
+        localY++;
+        cursorPos(localX, localY);
+    }
+
+
+}
+
+void drawFicha(int posX, int posY, int tipoFicha){
+    // Pasamos coordenadas globales a locales
+    int localX = posX + gameInterface_position_X + gameInterface_size_margin;
+    int localY = posY + gameInterface_position_Y + gameInterface_size_margin;
+
+
+    setColors(0, 14);
+    cursorPos(localX, localY);
+
+    switch (tipoFicha){
+        case 1:
+            // START L
+            for(int i = 0; i < 7; i++){
+                for(int j = 0; j < 5; j++){
+                    if(tetrominoe_1[i][j] == 1){
+                        printf("%c", gameFicha_ascii_model);
+                    }
+                }
+                printf("\n");
+                localY++;
+                cursorPos(localX, localY);
+            }
+            // END L
+        break;
+
+    }
+
+
+
+}
 
 void setColors(int texto, int fondo){
     colorTexto(texto, fondo);
 }
 
 void menu(){
-
-    //printf("*** ------------ *** iJosTris Admin Backend *** ------------ ***\n\n\n");
-
     //ventana(0, 0, 4, 3, 0);
 
 }
 
-void test(){
 
-}
 
 int main(){
     /* Para la correcta ejecución de este juego, debemos poner la consola en "fuente de mapa de bits" y 8x8 píxeles de tamaño */
     //Nuestra ventana molona bien grande
-    ventanaConsola(150, 100, "iJoStratego");
+    ventanaConsola(anchoVentana, altoVentana, "iJosTris");
 
     //Establecemos los colores iniciales
     setColors(0, 14);
 
-    drawScreen(10, 10);
+    pantallaJuego();
+    /*
+    for(int i = 1; i < 255; i++){
+        printf("%d -> %c \n", i, i);
+    }
+    */
+
+
 
     //menu();
 
+    getch();
     return 0;
 }
 
